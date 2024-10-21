bbaae31f957528278d3f1b4b4f280a59eea57c81
sofia-p <sofiapetrova1021@gmail.com>
2024-10-21 22:49:58 +0600

    DEV-17.Added design to game page

---
+++ src/components/GameBoard.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="game-board">
+    <div v-for="(cell, index) in cells" :key="index" class="box" @click="check(index)">
+      <span
+        :class="{
+          x: cell === 'X',
+          o: cell === 'O'
+        }"
+      >
+        {{ cell }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      cells: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+      currentPlayer: 'X',
+      showModal: true
+    }
+  },
+
+  methods: {
+    check(index) {
+      if (this.cells[index] === ' ') {
+        this.cells[index] = this.currentPlayer
+        this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X'
+      }
+    },
+    handleCloseModal() {
+      this.showModal = false
+    }
+  }
+}
+</script>
+
+<style>
+@font-face {
+  font-family: 'HomeVideoFont';
+  src: url('/assets/fonts/home_video/HomeVideo.ttf') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+
+.game-board {
+  display: grid;
+  grid-template-columns: repeat(3, 150px);
+  grid-template-rows: repeat(3, 150px);
+  background-image: url('/assets/frame.svg');
+}
+
+.box {
+  font-family: 'HomeVideoFont';
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 100px;
+  width: 150px;
+  height: 150px;
+  background-color: transparent;
+  border: transparent;
+}
+
+.x {
+  color: rgba(255, 74, 88, 1);
+}
+
+.o {
+  color: rgba(20, 224, 209, 1);
+}
+</style>
---
+++ src/views/GameBoardPage.vue
@@ -0,0 +1,53 @@
+<script>
+// import WaitingWindow from '@/components/WaitingWindow.vue'
+import GameBoard from '@/components/GameBoard.vue'
+
+export default {
+  components: {
+    GameBoard
+  }
+}
+</script>
+
+<template>
+  <!-- <WaitingWindow :isVisible="showModal" @close="handleCloseModal" /> -->
+  <!--Окно ожидания второго игрока-->
+  <div class="page">
+    <GameBoard />
+    <button class="leave-b" @click="$router.push('/')">leave the game</button>
+  </div>
+</template>
+
+<style>
+@font-face {
+  font-family: 'HomeVideoFont';
+  src: url('/assets/fonts/home_video/HomeVideo.ttf') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+
+.page {
+  display: grid;
+  justify-content: center;
+  align-items: center;
+  margin-top: 100px;
+}
+
+.leave-b {
+  font-family: 'HomeVideoFont';
+  background-color: transparent;
+  border: transparent;
+  color: white;
+  font-size: 40px;
+  margin-top: 100px;
+}
+
+.leave-b:hover {
+  font-size: 50px;
+  text-shadow:
+    0 0 10px rgba(255, 255, 255, 0.8),
+    0 0 20px rgba(255, 255, 255, 0.6),
+    0 0 30px rgba(255, 255, 255, 0.4),
+    0 0 40px rgba(255, 255, 255, 0.253);
+}
+</style>
