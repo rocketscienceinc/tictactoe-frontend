3c1001d08e171c24b62d1d58ef507b2a17e76cdb
sofia-p <sofiapetrova1021@gmail.com>
2024-11-27 21:57:09 +0600

    New layout of the Welcome page

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,17 @@
-<script>
-import { emit, ws, register } from '@/websocket'
-import config from './config'
-import appState from './state'
-
-appState.userId = localStorage.getItem(config.cookieName)
-
-ws.addEventListener('open', () => {
-  emit('connect', { player: { id: appState.userId } })
-})
-export default {
-  mounted() {
-    register('connect', (payload) => {
-      console.log('Получили сообщения от action connect', payload)
-      localStorage.setItem(config.cookieName, payload.player.id)
-      appState.userId = payload.player.id
-      if (payload.game !== undefined) {
-        appState.board = payload.game.board
-        appState.gameId = payload.game.id
-        appState.gameStatus = payload.game.status
-        this.$router.push('/game')
-      }
-    })
-  }
-}
-</script>
-
 <template>
-  <main><router-view></router-view></main>
+  <router-view></router-view>
 </template>
+<script></script>
 <style>
 html,
 body {
   margin: 0;
   padding: 0;
+  font-family: 'HomeVideoFont', sans-serif;
+}
+
+button {
+  font-family: 'HomeVideoFont', sans-serif;
 }
 @font-face {
   font-family: 'HomeVideoFont';
