d15868addaed12a9d978226f68b1e5452c2b7eda
sofia-p <sofiapetrova1021@gmail.com>
2024-12-26 16:11:45 +0600

    Fix public waiting window

--- src/components/publicWaitingForOpponent.vue
+++ src/components/publicWaitingForOpponent.vue
@@ -18,21 +18,21 @@ export default {
         'we select the best opponent',
         'searching searching searching'
       ],
-      changingText: 'waiting for the opponent' // Начальный текст
+      changingText: 'waiting for the opponent',
+      currentIndex: 0
     }
   },
+
   mounted() {
     this.changeText()
   },
+
   methods: {
     changeText() {
       setInterval(() => {
-        this.changingText = this.getRandomText() // Получаем случайный текст
-      }, 6000) // 6000 миллисекунд = 6 секунд
-    },
-    getRandomText() {
-      const randomIndex = Math.floor(Math.random() * this.texts.length) // Генерация случайного индекса
-      return this.texts[randomIndex] // Возвращаем текст по случайному индексу
+        this.changingText = this.texts[this.currentIndex]
+        this.currentIndex = (this.currentIndex + 1) % this.texts.length
+      }, 4000)
     }
   }
 }
